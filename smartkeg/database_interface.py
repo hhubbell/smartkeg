241d9fabd88e9f473bfb996cd5e73c38c1393be6
Harrison Hubbell <hhubbell@uvm.edu>
2014-10-05 10:52:52 -0400

    [UPD] Error Handling

--- smartkeg/database_interface.py
+++ smartkeg/database_interface.py
@@ -6,21 +6,30 @@
 #               of the project does not allow users to delete records via this
 #               interface. In fact, the database user should not only have 
 #               INSERT and SELECT permissions.
-# TODO:         Gracefully handle errors, particularly:
-#                   > Failed database connection attempts
-#                   > Failed INSERT transactions
-#                   > Failed SELECT transactions
 # ----------------------------------------------------------------------------
 
 import MySQLdb
 
-class DatabaseInterface(object):    
+class DatabaseInterface(object):
     def __init__(self, addr, dbn, user, pwd):
-        self.conn = MySQLdb.connect(addr, user, pwd, dbn)
-
+        self.connect(addr, dbn, user, pwd)
+        
     def __exit__(self):
         self.conn.close()
 
+    def connect(self, addr, dbn, user, pwd):
+        """
+        @Author:        Harrison Hubbell
+        @Created:       10/05/2014
+        @Description:   Attempts to connect to the MySQL database.  Handles 
+                        errors if attempt fails.
+        """
+        try:
+            self.conn = MySQLdb.connect(addr, user, pwd, dbn, cursorclass=MySQLdb.cursors.DictCursor)
+        except OperationalError:
+            # XXX Log to file
+            print "Could not connect to the database"
+            
     def prepare(self):
         """
         @Author:        Harrison Hubbell
@@ -36,8 +45,11 @@ class DatabaseInterface(object):
         @Description:   Makes an INSERT transaction on the database
         """
         self.prepare()
-        self.cur.executemany(query, params)
-        self.database.commit()
+        try:
+            self.cur.executemany(query, params)
+            self.database.commit()
+        except:
+            self.database.rollback()
 
     def SELECT(self, query, params):
         """
@@ -46,5 +58,10 @@ class DatabaseInterface(object):
         @Description:   Makes a SELECT transaction on the database, returns result
         """
         self.prepare()
-        self.cur.execute(query, params)
-        return self.cur.fetchall()
+        try:
+            self.cur.execute(query, params)
+            res = self.cur.fetchall()
+        except:
+            self.database.rollback()
+        
+        return res
