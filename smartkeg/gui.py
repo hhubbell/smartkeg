9d318a3b8c4d6227cf4e5f5835d4adec44c4cf1e
Christopher Young <cwyoung11@comcast.net>
2014-11-05 18:34:02 -0500

    playing with titles and colors

--- smartkeg/gui.py
+++ smartkeg/gui.py
@@ -8,35 +8,55 @@
 #----------------------------------------------------------------------------
 
 # import the wxPython GUI package
+import os
 import wx
 
-
-# Initialize frame
-import wx
 
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(900,600))
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        wx.Frame.__init__(self, parent, title=title, size=(1000,600))
+        #self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A StatusBar in the bottom of the window
 
         # Setting up the menu.
         filemenu= wx.Menu()
 
-        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About","Get some info")
+        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit", "Quit the GUI")
 
         # Creating the menubar.
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        
+        
+        #panel additions
+        titlePanel = wx.Panel(self)
+        titlePanel.SetBackgroundColour((10,110,186))                #blue
+        
+        titleText= wx.StaticText(titlePanel, label = "SmartKeg")    #make text
+        titleFont = wx.Font(140, wx.SWISS, wx.SLANT, wx.LIGHT)      #create font
+        titleText.SetFont(titleFont)                                #add font to text
+        titleText.SetForegroundColour((255,247,0))                  #yellow
+        
+        self.quote = titleText                                      #show text
+
         self.Show(True)
 
+    def OnAbout(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, "Welcome to the future of beverage consumption.", "About SmartKeg", wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
+
+    def OnExit(self,e):
+        self.Close(True)  # Close the frame.
+
 app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
+frame = MainWindow(None, "Welcome to SmartKeg")
 app.MainLoop()
-
-
-
-
