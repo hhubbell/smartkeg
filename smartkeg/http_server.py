3fcfc9886beb31c3021f4fac50547f719ec8f9e4
Harrison Hubbell <hhubbell@uvm.edu>
2014-11-07 12:21:41 -0500

    [REM] Whitespace

--- smartkeg/http_server.py
+++ smartkeg/http_server.py
@@ -15,7 +15,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     HTTP_OK  = 200
     HTTP_FILE_NOT_FOUND = 404
     _BASE_DIR = '/usr/local/src/smartkeg/'
-    _CONFIG_PATH = _BASE_DIR + 'etc/config.cfg'    
+    _CONFIG_PATH = _BASE_DIR + 'etc/config.cfg'
     _SERVER_DIR = _BASE_DIR + 'srv/'
     _INDEX = 'index.html'
 
@@ -23,10 +23,10 @@ class RequestHandler(BaseHTTPRequestHandler):
         """ 
         @Author:        Harrison Hubbell
         @Created:       09/01/2014
-        @Description:   Return content type based on file.  Essentially 
+        @Description:   Return content type based on file.  Essentially
                         just a lot of if statements
         """
-        if req.endswith('.css'):    return 'text/css'        
+        if req.endswith('.css'):    return 'text/css'
         if req.endswith('.html'):   return 'text/html'
         if req.endswith('.ico'):    return 'image/x-icon'
         if req.endswith('.js'):     return 'application/javascript'
@@ -39,7 +39,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         @Created:       10/05/2014
         @Description:   Locates the requested page.
         """
-        if self.path[1:] is '': 
+        if self.path[1:] is '':
             page = self._SERVER_DIR + self._INDEX
         else:
             page = self._SERVER_DIR + self.path[1:]
@@ -54,7 +54,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                         log server actions to a file.
         """
         logger = SmartkegLogger(self._CONFIG_PATH)
-        logger.log(['[HTTP Server]', args])
+        logger.log(['[HTTP Server]', list(args)])
 
     # --------------------
     # HTTP METHODS
@@ -80,12 +80,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle Requests in a Seperate Thread."""
-    
 
-class SmartkegHTTPServer:    
+
+class SmartkegHTTPServer:
     def __init__(self, host, port):
         self.httpd = ThreadedHTTPServer((host, port), RequestHandler)
-        
+
     def main(self):
         self.httpd.serve_forever()
 
@@ -98,6 +98,6 @@ if __name__ == '__main__':
     cfg.read(_CONFIG_PATH)
     host = cfg.get(_HEADER, 'host')
     port = cfg.getint(_HEADER, 'port')
-        
+
     server = SmartkegHTTPServer(host, port)
     server.main()
