4bfca1f3bb9c43ebe794490f22d0a54ba387121e
Harrison Hubbell <hhubbell@uvm.edu>
2015-07-10 17:24:39 -0400

    Allow user to specify a host, or default to localhost

--- gui.py
+++ gui.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python2
-#----------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 # Name:         gui.py
 # Author:       Christopher Young
 # Created:      11/1/14
-# Description:  Create a webkit enabled GUI to render the web interface in a
-#               special purpose window.
-#----------------------------------------------------------------------------
+# Description:  Create a webkit enabled GUI to render the web interface in 
+#               a special purpose window.  Render the host provided as the 
+#               first if applicable, otherwise render localhost.
+#--------------------------------------------------------------------------
 
+import sys
 import gtk
 import webkit
 import gobject
 
+DEFAULT_HOST = 'http://localhost'
+
 if __name__ == '__main__':
-    HOST = 'http://localhost'
+    host = sys.argv[1] if len(sys.argv) > 2 else DEFAULT_HOST
 
     gobject.threads_init()
     window = gtk.Window()
     browser = webkit.WebView()
-    browser.open(HOST)
+    browser.open(host)
     window.add(browser)
     window.show_all()
 
