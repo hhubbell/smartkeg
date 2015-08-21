e901be25a7d2bf7319931603b0d3eff9c52fffa4
Harrison Hubbell <hhubbell@uvm.edu>
2015-08-21 15:41:02 -0400

    Total removal of process.py

--- smartkeg/led_display.py
+++ smartkeg/led_display.py
@@ -1,21 +1,21 @@
-# ----------------------------------------------------------------------------
+#
 # Filename:     led_display.py
 # Author:       Harrison Hubbell
 # Date:         08/31/2014
 # Description:  Manages lighting the LED display on the kegerator based on
-#               the amount of beer left in the keg.  The LEDs are multiplexed
-#               to limit the amount of GPIO pins required to light each row,
-#               as well as saving some energy over lighting all rows at once;
-#               approximately 120 mA.  Multiplexing is expensive however, and
-#               the perfomance of the loop - starting the time a row lights
-#               and stopping when the row is lit again - must not fall below
-#               50Hz - to prevent noticable light flicker.
-# ----------------------------------------------------------------------------
+#               the amount of beer left in the keg.  The LEDs are 
+#               multiplexed to limit the amount of GPIO pins required to 
+#               light each row, as well as saving some energy over lighting
+#               all rows at once; approximately 120 mA.  Multiplexing is 
+#               expensive however, and the perfomance of the loop - starting
+#               the time a row lights and stopping when the row is lit
+#               again - must not fall below 50Hz - to prevent noticable light
+#               flicker.
+#
 
-from process import ChildProcess
 import time
 
-class LEDDisplay(ChildProcess):
+class LEDDisplay(object):
     ROW = [
         [15, 13, 11],
         [13, 15, 11],
@@ -24,11 +24,10 @@ class LEDDisplay(ChildProcess):
     ]
 
     def __init__(self, pipe, gpio):
-        super(LEDDisplay, self).__init__(pipe)
         self.GPIO = gpio
-        self.light = 0
+        self.rows = 0
 
-    def light_row(self, row):
+    def light(self, row):
         """
         @Author:        Harrison Hubbell
         @Created:       08/31/2014
@@ -42,18 +41,17 @@ class LEDDisplay(ChildProcess):
         self.GPIO.output(row[0], self.GPIO.HIGH)
         self.GPIO.output(row[1], self.GPIO.LOW)
 
-    def set_rows(self):
+    def get(self):
         """
         @Author:        Harrison Hubbell
         @Created:       10/05/2014
         @Description:   Receives the number of rows to light and updates
                         if there is a new value.
         """
-        rows = self.proc_poll_recv()
-        if rows is not self.light:
-            self.light = rows
-
-    def main(self):
+        if self.pipe.poll():
+            self.rows = self.pipe.recv()
+        
+    def run(self):
         """
         @Author:        Harrison Hubbell
         @Created:       08/31/2014
@@ -61,12 +59,12 @@ class LEDDisplay(ChildProcess):
                         which is received from its parent proc.
         """
         while True:
-            self.set_rows()
-            if self.light:
-                i = self.light - 1
-                while i >= 0:
-                    self.light_row(self.ROW[i])
-                    i -= 1
+            self.get()
+            
+            i = self.rows - 1
+            while i >= 0:
+                self.light(self.ROW[i])
+                i -= 1
 
             # This prevents the CPU from running out of control.  If this
             # causes a noticable flicker in the display, it will have to 
