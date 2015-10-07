046f0dcbbc5b9c9e75902e646b5cc1f4cf458a75
Harrison Hubbell <hhubbell@uvm.edu>
2015-10-06 20:44:26 -0400

    Rename database_interface.py to database.py

--- smartkeg/__init__.py
+++ smartkeg/__init__.py
@@ -1,4 +1,4 @@
-from .database_interface import DatabaseInterface
+from .database import DatabaseInterface
 from .peripherals import TermperatureSensorManager, FlowMeterManager
 from .http import HTTPServerManager
 from .model import TimeSeriesRegression
