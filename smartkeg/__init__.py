749ff10d4f284bb99539f93f7c57aa4757e6ec6a
Harrison Hubbell <hhubbell@uvm.edu>
2015-10-06 20:06:04 -0400

    Rename http_server.py to http.py

--- smartkeg/__init__.py
+++ smartkeg/__init__.py
@@ -1,6 +1,6 @@
 from .database_interface import DatabaseInterface
 from .temperature_sensor import TemperatureSensorManager
-from .http_server import HTTPServerManager
+from .http import HTTPServerManager
 from .flow_meter import FlowMeterManager
 from .model import TimeSeriesRegression
 from . import query
